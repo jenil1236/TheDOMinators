7767a289e97eb9e0b778d34030e8e6fbd0768f10
jenil1236 <jenilp2146@gmail.com>
2025-07-10 00:21:25 +0530

    timetable admin backend

---
+++ Backend/models/stop.js
@@ -0,0 +1,39 @@
+const mongoose=require("mongoose");
+const {Schema}=mongoose;
+
+const stopSchema= new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    lat:{
+        type:Number,
+        required:true
+    },
+    lng:{
+        type:Number,
+        required:true
+    },
+    details:{
+        type:String,
+        required:true
+    },
+    timetable:[{
+        bus_number:{
+            type:String,
+            required:true
+        },
+        destination:{
+            type:String,
+            required:true
+        },
+        time:{
+            type:String,
+            required:true
+        }
+    }]
+});
+
+const Stop=mongoose.model("Stop",stopSchema);
+
+module.exports=Stop;
---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import Sidebar from './Sidebar'
+import MapComponent from './MapComponent'
+import { useState, useEffect } from 'react';
+
+function App() {
+  const url = import.meta.env.VITE_URL;
+  const [busStops, setStops] = useState([]);
+  useEffect(() => {
+    fetch(`${url}`)
+      .then(res => res.json())
+      .then(setStops);
+  }, []);
+  const [focus, setFocus] = useState({ lat: 21.1702, lng: 72.8311 })
+  return (
+    <>
+      <div className="layout-container">
+        <Sidebar busStops={busStops} setStops={setStops} focus={focus} setFocus={setFocus} />
+        <MapComponent busStops={busStops} focus={focus} />
+      </div>
+    </>
+  )
+}
+
+export default App
