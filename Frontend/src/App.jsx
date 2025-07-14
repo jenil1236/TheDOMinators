6d5ea0d6af5574863d02811c5dd1eb84b0954e25
jenil1236 <jenilp2146@gmail.com>
2025-07-15 00:56:02 +0530

    Chatbot complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // import BusStopMarkers from "./user/BusStopMarkers/BusStopMarkers"
 // import BusStopMarkers from "./admin/BusStopMarkers/BusStopMarkers"
-import ChatBot from "./user/ChatBot/ChatBot"
+import ChatBot from "./user/ChatBot/Bot"
 // import Notification from "./user/Announcements/Notification"
 function App() {
   return (
