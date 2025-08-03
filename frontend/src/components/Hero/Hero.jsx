6436b0b4d02b6a1e1e977c6430c412ae86aba7d1
jenil1236 <jenilp2146@gmail.com>
2025-08-03 12:24:53 +0530

    15

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import './Hero.css';
+import herotransport from '/src/assets/videos/hero-transport.mp4';
 
 const Hero = () => {
   return (
     <div className="hero-section">
       <div className="video-overlay" />
       <video autoPlay muted loop className="hero-video">
-        <source src="src/assets/videos/hero-transport.mp4" type="video/mp4" />
+        <source src={herotransport} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      
-<motion.div 
-  className="hero-content"
-  initial={{ opacity: 0, y: 20 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 0.8 }}
->
-  <div className="blurred-box">
-    <motion.h1 
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.3, duration: 0.8 }}
-    >
-      Revolutionizing <span className="highlight">Urban Mobility</span>
-    </motion.h1>
-    <motion.p
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.6, duration: 0.8 }}
-    >
-      Seamless transportation solutions for campuses and cities
-    </motion.p>
-      </div>
 
-</motion.div>
+      <motion.div
+        className="hero-content"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <div className="blurred-box">
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.8 }}
+          >
+            Revolutionizing <span className="highlight">Urban Mobility</span>
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6, duration: 0.8 }}
+          >
+            Seamless transportation solutions for campuses and cities
+          </motion.p>
+        </div>
 
-      
+      </motion.div>
+
+
       <div className="transport-icons">
         {['🚇', '🚌', '🚲', '🚗', '🛴'].map((icon, index) => (
           <motion.div
